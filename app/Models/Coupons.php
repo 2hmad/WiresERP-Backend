125aa2ea252dd1ab2f049c9a3a553097cb93cec8
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-07-21 02:38:10 +0200

    discount coupons table

---
+++ app/Models/Coupons.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coupons extends Model
+{
+    use HasFactory;
+    public $table = "discount_coupons";
+    protected $fillable = [
+        'code',
+        'amount',
+        'expiry_date',
+        'section',
+        'client_id',
+        'category_id',
+        'product_id',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
---
+++ database/migrations/2022_07_21_002814_create_discount_coupons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_coupons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('company_id');
+            $table->string('code');
+            $table->text('amount');
+            $table->date('expire_date');
+            $table->date('section');
+            $table->date('client_id')->nullable();
+            $table->date('category_id')->nullable();
+            $table->date('product_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount_coupons');
+    }
+};
