efbb15f3a6d621696d314bf54f9a4948b119e11d
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-07-24 19:15:40 +0200

    update logo and stamp

---
+++ database/migrations/2022_07_24_171036_create_sale_bills_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_bills', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('company_id');
+            $table->unsignedInteger('client_id');
+            $table->unsignedInteger('bill_number');
+            $table->timestamp('date_time');
+            $table->unsignedInteger('warehouse_id');
+            $table->text('taxe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_bills');
+    }
+};
